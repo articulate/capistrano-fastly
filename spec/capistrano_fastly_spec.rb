05621155759774764a22b3e8c9c6ada725980b0b
Dave Konopka <dkonopka@articulate.com>
2013-04-23 17:17:52 -0400

    Test for existence of purge_all task

--- spec/capistrano_fastly_spec.rb
+++ spec/capistrano_fastly_spec.rb
@@ -1,5 +1,13 @@
 require 'spec_helper'
 
 describe Capistrano::Fastly do
-    pending "YOOOOOOO!"
+    before do
+        @configuration = Capistrano::Configuration.new
+        Capistrano::Fastly.load_into(@configuration)
+    end
+
+    it 'provides fastly:purge_all' do
+        @configuration.find_task('fastly:purge_all').should_not == nil
+    end
+
 end
