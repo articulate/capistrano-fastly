699cf4193d103c8fb15aa36bea52d31496c933a4
Dave Konopka <dkonopka@articulate.com>
2013-04-23 17:08:47 -0400

    Rough out code structure

--- lib/capistrano-fastly.rb
+++ lib/capistrano-fastly.rb
@@ -1,7 +1,21 @@
-require "capistrano-fastly/version"
+require 'capistrano'
+require 'capistrano-fastly/version'
 
 module Capistrano
   module Fastly
-    # Your code goes here...
+    def self.load_into(configuration)
+      configuration.load do
+        desc 'Purge all Fastly cache'
+        namespace :fastly do
+          task :purge_all do
+            logger.info('Purged. Baaaaaaarf.')
+          end
+        end
+      end
+    end
   end
+end
+
+if Capistrano::Configuration.instance
+  Capistrano::Fastly.load_into(Capistrano::Configuration.instance)
 end
