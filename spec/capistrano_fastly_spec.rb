3dac5aad7118d20ecf188181c91ee3c9e6410fed
Dave Konopka <dkonopka@articulate.com>
2013-04-23 17:09:03 -0400

    Basic rspec setup

---
+++ spec/capistrano_fastly_spec.rb
@@ -0,0 +1,5 @@
+require 'spec_helper'
+
+describe Capistrano::Fastly do
+    pending "YOOOOOOO!"
+end
---
+++ spec/spec_helper.rb
@@ -0,0 +1,8 @@
+require 'rubygems'
+require 'bundler/setup'
+
+require 'capistrano-fastly'
+
+RSpec.configure do |config|
+  
+end
